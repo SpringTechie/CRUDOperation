60283ea0c05b3835be00270db258864d2aeba98d
springtechie <springframeworkdeveloper@gmail.com>
2025-02-16 08:11:49 +0530

    Fixed Test cases

--- src/test/java/com/springtechie/CrudOperationApplicationTests.java
+++ src/test/java/com/springtechie/CrudOperationApplicationTests.java
@@ -1,13 +1,10 @@
 package com.springtechie;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CrudOperationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
