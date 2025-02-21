268646ed7b9703d47d5d2bf929d170774726fd8c
springtechie <springframeworkdeveloper@gmail.com>
2025-02-21 06:54:14 +0530

    PR_1:Swagger Impl

--- src/main/java/com/springtechie/controller/ProductController.java
+++ src/main/java/com/springtechie/controller/ProductController.java
@@ -1,18 +1,18 @@
 package com.springtechie.controller;
 
 import com.springtechie.entity.Product;
-import com.springtechie.impl.ProductServiceImpl;
 import com.springtechie.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/v1/api")
+@Tag(name="Product-Controller",description = "This controller has product related API's")
 public class ProductController {
 
     private final ProductService productService;
@@ -24,6 +24,11 @@ public class ProductController {
 
     // to fetch the product
     @GetMapping("/get/id/{id}")
+    @Operation(summary = "Find product By Id",description = "This helps to find the product by Product Id")
+    @Parameter(name="id",description = "Enter Product Id",example = "1")
+    @ApiResponse(responseCode = "200",description = "ProductFound")
+    @ApiResponse(responseCode = "204",description = "No Product Found with Id")
+    @ApiResponse(responseCode = "400",description = "Bad Request")
     public Product getProduct(@PathVariable int id) {
        return productService.getProduct(id);
     }
